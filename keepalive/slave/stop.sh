00538ad44ec0ed9e24b5f68fbd709030bab8e8a6
diggzhang <diggzhang@gmail.com>
2020-07-27 18:28:05 +0800

    Update stop.sh

--- keepalive/slave/stop.sh
+++ keepalive/slave/stop.sh
@@ -15,26 +15,26 @@ sleep 1
 M_File2=$(mysql -uroot -p123456 -e "show master status\G" | awk -F': ' '/File/{print $2}')
 M_Position2=$(mysql -uroot -p123456 -e "show master status\G" | awk -F': ' '/Position/{print $2}')
 
-i=1
+# i=1
 
-while true
-do
+# while true
+# do
 
-if [ $M_File1 = $M_File1 ] && [ $M_Position1 -eq $M_Position2 ]
-then
-   echo "ok"
-   break
-else
-   #sleep 1
+# if [ $M_File1 = $M_File1 ] && [ $M_Position1 -eq $M_Position2 ]
+# then
+#    echo "ok"
+#    break
+# else
+#    #sleep 1
 
-   if [ $i -gt 20 ]
-   then
-      break
-   fi
-   continue
-   let i++
-fi
-done
+#    if [ $i -gt 20 ]
+#    then
+#       break
+#    fi
+#    continue
+#    let i++
+# fi
+# done
 
 
 curl 'https://oapi.dingtalk.com/robot/send?access_token=2c0247a604d7201cc804c67fff097405efadc5b7cb3e611437c51d5dc9e4d4bf' \
